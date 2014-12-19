306c9b55dc7b3567411f538c66034827b58b39bc
Giovan Gentile <giovan_gentile@hotmail.com>
2014-12-19 07:59:15 -0500

    Initial Commit

---
+++ User Data Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  User Data Challenge Solution
+//
+//  Created by Giovan Gentile on 2014-12-19.
+//  Copyright (c) 2014 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ User Data Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  User Data Challenge Solution
+//
+//  Created by Giovan Gentile on 2014-12-19.
+//  Copyright (c) 2014 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
